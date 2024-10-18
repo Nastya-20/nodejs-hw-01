c0b5862e129d7859196e4243294fdb1085eea0f7
Your NameNastya-20 <asyataa2016@gmail.com>
2024-10-18 22:47:09 +0300

    add

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
-import { PATH_DB } from '../constants/constants.js';
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../contacts/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        return JSON.parse(data);
+    }catch (err) {
+        console.error('Error reading contacts:', err);
+        throw err;
+    }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,12 @@
-import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
+import { PATH_DB } from '../contacts/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) => {
+    try {
+        const data = JSON.stringify(contacts, null, 2);
+        await fs.writeFile(PATH_DB, data, 'utf-8');
+    } catch (err) {
+        console.error('Error writing contacts:', err);
+        throw err;
+    }
+};
